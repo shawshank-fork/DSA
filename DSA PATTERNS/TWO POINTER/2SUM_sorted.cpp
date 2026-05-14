053472a0d4b5855df45b675815b5092e47bfa3be
shawshank <shashanklakhaiyar71@gmail.com>
2026-05-14 10:34:49 +0530

    Understanding DSA patterns

---
+++ DSA PATTERNS/TWO POINTER/2SUM_sorted.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include <algorithm>
+#include <numeric>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int i = 0, j = n - 1;
+        while(i < j) {
+            if(numbers[i] + numbers[j] == target){
+                return {i + 1, j + 1};
+            }
+            else if(numbers[i] + numbers[j] > target){
+                j--;
+            }
+            else if(numbers[i] + numbers[j] < target) {
+                i++;
+            }
+        }
+        return {};
+    }
+};
