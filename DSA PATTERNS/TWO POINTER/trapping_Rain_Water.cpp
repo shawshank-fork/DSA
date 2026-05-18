20ac343dcacd3a08b387fada1fcd0f41ea4ddec9
shawshank <shashanklakhaiyar71@gmail.com>
2026-05-18 20:00:33 +0530

    solved trapping rain water - leet_hard

---
+++ DSA PATTERNS/TWO POINTER/trapping_Rain_Water.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include <algorithm>
+#include <numeric>
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lmax = 0, rmax = 0, total = 0;
+        int l = 0, r = n-1; 
+
+        while( l < r) {
+            if(height[l]  <= height[r]) {
+                if(lmax > height[l]) {
+                    total += lmax - height[l];
+                }
+                else {
+                    lmax = height[l];
+                   
+                }
+                l = l + 1;
+            }
+            else {
+                if(rmax > height[r]) {
+                    total += rmax - height[r];
+                }
+                else {
+                    rmax = height[r];
+                    
+                }
+                r = r - 1;
+            }
+        }
+        return total;
+    }
+};
