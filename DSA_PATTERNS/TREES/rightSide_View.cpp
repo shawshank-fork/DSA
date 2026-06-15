138fae87edaea7c73eab6a021e02537661d309e0
shawshank <shashanklakhaiyar71@gmail.com>
2026-06-15 11:58:39 +0530

    right side view of bst, using dfs traversal
    always check the right side of the node first and then move to the left

---
+++ DSA_PATTERNS/TREES/rightSide_View.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include <algorithm>
+#include <numeric>
+#include<stack>
+#include<string>
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    vector<int> ans;
+
+    void dfs(TreeNode* root, int depth) {
+        if(root == NULL) return;
+
+        if(depth == ans.size()){
+            ans.push_back(root->val);
+        }
+
+        dfs(root->right, depth + 1);
+        dfs(root->left, depth + 1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root,0);
+        return ans;
+    }
+};
