1f80b51e0b7d8abed957080568a253a110cc49c2
shawshank <shashanklakhaiyar71@gmail.com>
2026-06-20 16:39:19 +0530

    Finding max Area of island
    algo used - BFS, TC - O(n * m)

---
+++ DSA_PATTERNS/GRAPHS/maxArea_Island.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+
+class Solution {
+
+    int bfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int area = 1;
+
+        vis[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
+                    vis[nrow][ncol] = 1;
+                    area++;
+                    q.push({nrow, ncol});
+                }
+
+            }
+        }
+        return area;
+    }
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxArea = 0;
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+                if(grid[row][col] == 1 && !vis[row][col]) {
+                    maxArea = max(maxArea,bfs(row,col,vis,grid));
+                }
+            }
+        }
+        return maxArea;
+    }
+};
+//TC - O(m * n)
