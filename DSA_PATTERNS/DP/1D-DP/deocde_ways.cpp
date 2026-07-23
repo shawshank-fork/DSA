5729db8c0699f09ef90f55b1ca49ed71bfa3dcaa
shawshank <shashanklakhaiyar71@gmail.com>
2026-07-23 12:28:26 +0530

    decode ways
    solve by dp, by taking dp[i] as number ways to decode the substring from i to n
    
    solve for the cases of taking only one digit - dp[i + 1] or taking 2 digits - dp[i+2], we will be solving to from the right to left as for our dp it depends on the future states

---
+++ DSA_PATTERNS/DP/1D-DP/deocde_ways.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<climits>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        int next = 1;
+        int next2 = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            int curr = 0;
+            if(s[i] != '0'){
+                curr = next;
+                if(i + 1 < n && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')) {
+                    curr += next2;
+                }
+            }
+
+            
+            next2 = next;
+            next = curr;
+        }
+        return next;;
+    }
+};
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+
+            dp[i] = dp[i + 1];
+
+            if(i + 1 < n && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6')) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+};
